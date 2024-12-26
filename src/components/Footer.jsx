be706027acbbebb7571e90755f6baaadc3174155
parthosarothi46 <parthosarothi46@gmail.com>
2024-12-26 20:26:26 +0600

    Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,15 @@ import { Input } from "@/components/ui/input";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-200 py-10">
+    <footer className="bg-black dark:bg-white py-10">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-6">
           {/* Brand Section */}
           <div>
-            <h1 className="text-2xl font-bold text-white">TutorBook</h1>
-            <p className="text-sm mt-2 text-gray-400">
+            <h1 className="text-2xl font-bold text-white dark:text-black">
+              TutorBook
+            </h1>
+            <p className="text-sm mt-2 text-white dark:text-black">
               Empowering learners worldwide with the best tutors.
             </p>
           </div>
@@ -17,8 +19,10 @@ export default function Footer() {
           {/* Links Section */}
           <div className="flex flex-wrap gap-6 text-sm">
             <div>
-              <h2 className="font-bold text-white mb-2">Company</h2>
-              <ul>
+              <h2 className="font-bold text-white dark:text-black mb-2">
+                Company
+              </h2>
+              <ul className="text-white dark:text-black">
                 <li>
                   <a href="/about" className="hover:underline">
                     About Us
@@ -37,8 +41,10 @@ export default function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className="font-bold text-white mb-2">Resources</h2>
-              <ul>
+              <h2 className="font-bold text-white dark:text-black mb-2">
+                Resources
+              </h2>
+              <ul className="text-white dark:text-black">
                 <li>
                   <a href="/blog" className="hover:underline">
                     Blog
@@ -60,14 +66,21 @@ export default function Footer() {
 
           {/* Newsletter Section */}
           <div className="w-full md:w-auto">
-            <h2 className="font-bold text-white mb-2">Stay Updated</h2>
+            <h2 className="font-bold text-white dark:text-black mb-2">
+              Stay Updated
+            </h2>
             <div className="flex items-center gap-2">
               <Input
                 type="email"
                 placeholder="Enter your email"
-                className="flex-1 bg-gray-800 border-gray-700 text-gray-200"
+                className="flex-1 text-white dark:text-black"
               />
-              <Button variant="default">Subscribe</Button>
+              <Button
+                variant="default"
+                className="bg-white dark:bg-black text-black dark:text-white hover:text-black hover:bg-white"
+              >
+                Subscribe
+              </Button>
             </div>
           </div>
         </div>
@@ -81,30 +94,55 @@ export default function Footer() {
               href="https://facebook.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-white"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
+                x="0px"
+                y="0px"
+                width="100"
+                height="100"
+                viewBox="0 0 48 48"
+                className="w-10 h-10"
               >
-                <path d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.406.593 24 1.325 24H12v-9.294H9.294V11.53H12V9.294c0-2.673 1.632-4.14 4.016-4.14 1.14 0 2.126.084 2.412.122v2.794h-1.655c-1.298 0-1.55.618-1.55 1.524v2H18l-.706 3.177h-2.294V24h4.676c.732 0 1.325-.593 1.325-1.325V1.325C24 .593 23.406 0 22.675 0z" />
+                <linearGradient
+                  id="Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1"
+                  x1="9.993"
+                  x2="40.615"
+                  y1="9.993"
+                  y2="40.615"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop offset="0" stop-color="#2aa4f4"></stop>
+                  <stop offset="1" stop-color="#007ad9"></stop>
+                </linearGradient>
+                <path
+                  fill="url(#Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1)"
+                  d="M24,4C12.954,4,4,12.954,4,24s8.954,20,20,20s20-8.954,20-20S35.046,4,24,4z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46 c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.452 C21.988,43.9,22.981,44,24,44c0.921,0,1.82-0.084,2.707-0.204V29.301z"
+                ></path>
               </svg>
             </a>
             <a
               href="https://twitter.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-white"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
+                x="0px"
+                y="0px"
+                width="100"
+                height="100"
+                viewBox="0 0 48 48"
+                className="h-10 w-10"
               >
-                <path d="M24 4.56c-.883.392-1.83.656-2.825.774 1.017-.611 1.798-1.574 2.165-2.723-.951.564-2.005.974-3.127 1.195a5.44 5.44 0 0 0-9.261 4.962c-4.52-.226-8.528-2.394-11.205-5.69a5.44 5.44 0 0 0-.735 2.733c0 1.887.962 3.554 2.425 4.532a5.432 5.432 0 0 1-2.463-.682v.068a5.44 5.44 0 0 0 4.362 5.331 5.481 5.481 0 0 1-2.458.093 5.44 5.44 0 0 0 5.082 3.773A10.914 10.914 0 0 1 0 19.54a15.38 15.38 0 0 0 8.317 2.437c9.988 0 15.446-8.28 15.446-15.446 0-.235-.006-.469-.016-.702A11.062 11.062 0 0 0 24 4.56z" />
+                <path
+                  fill="#03A9F4"
+                  d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0.022,0,0.055,0,0.086c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945C39.763,15.197,41.013,13.905,42,12.429"
+                ></path>
               </svg>
             </a>
             <a
@@ -115,17 +153,71 @@ export default function Footer() {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
+                x="0px"
+                y="0px"
+                width="100"
+                height="100"
+                viewBox="0 0 64 64"
+                className="h-10 w-10"
               >
-                <path d="M12 2.163c3.2 0 3.584.013 4.85.07 1.366.062 2.633.34 3.608 1.316.975.976 1.253 2.243 1.316 3.609.057 1.266.07 1.65.07 4.85s-.013 3.584-.07 4.85c-.062 1.366-.34 2.633-1.316 3.609-.976.975-2.243 1.253-3.608 1.316-1.266.057-1.65.07-4.85.07s-3.584-.013-4.85-.07c-1.366-.062-2.633-.34-3.609-1.316-.975-.976-1.253-2.243-1.316-3.608-.057-1.266-.07-1.65-.07-4.85s.013-3.584.07-4.85c.062-1.366.34-2.633 1.316-3.609.976-.975 2.243-1.253 3.608-1.316C8.416 2.176 8.8 2.163 12 2.163m0-2.163C8.737 0 8.332.015 7.052.072 5.768.129 4.657.397 3.763 1.29.346 4.707.015 8.33 0 12c.015 3.27.346 6.893 3.763 10.31C7.18 23.985 10.803 24.315 12 24.315c1.197-.015 4.82-.345 8.237-3.763 3.417-3.417 3.747-7.04 3.763-8.237.015-1.197.345-4.82-3.763-8.237C20.34.346 16.717.015 12 0z" />
+                <radialGradient
+                  id="TGwjmZMm2W~B4yrgup6jda_119026_gr1"
+                  cx="32"
+                  cy="32.5"
+                  r="31.259"
+                  gradientTransform="matrix(1 0 0 -1 0 64)"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop offset="0" stop-color="#efdcb1"></stop>
+                  <stop offset="0" stop-color="#f2e0bb"></stop>
+                  <stop offset=".011" stop-color="#f2e0bc"></stop>
+                  <stop offset=".362" stop-color="#f9edd2"></stop>
+                  <stop offset=".699" stop-color="#fef4df"></stop>
+                  <stop offset="1" stop-color="#fff7e4"></stop>
+                </radialGradient>
+                <path
+                  fill="url(#TGwjmZMm2W~B4yrgup6jda_119026_gr1)"
+                  d="M58,54c-1.1,0-2-0.9-2-2s0.9-2,2-2h2.5c1.9,0,3.5-1.6,3.5-3.5S62.4,43,60.5,43H50c-1.4,0-2.5-1.1-2.5-2.5	S48.6,38,50,38h8c1.7,0,3-1.3,3-3s-1.3-3-3-3H42v-6h18c2.3,0,4.2-2,4-4.4c-0.2-2.1-2.1-3.6-4.2-3.6H58c-1.1,0-2-0.9-2-2s0.9-2,2-2	h0.4c1.3,0,2.5-0.9,2.6-2.2c0.2-1.5-1-2.8-2.5-2.8h-14C43.7,9,43,8.3,43,7.5S43.7,6,44.5,6h3.9c1.3,0,2.5-0.9,2.6-2.2	C51.1,2.3,50,1,48.5,1H15.6c-1.3,0-2.5,0.9-2.6,2.2C12.9,4.7,14,6,15.5,6H19c1.1,0,2,0.9,2,2s-0.9,2-2,2H6.2c-2.1,0-4,1.5-4.2,3.6	C1.8,16,3.7,18,6,18h2.5c1.9,0,3.5,1.6,3.5,3.5S10.4,25,8.5,25H5.2c-2.1,0-4,1.5-4.2,3.6C0.8,31,2.7,33,5,33h17v11H6	c-1.7,0-3,1.3-3,3s1.3,3,3,3l0,0c1.1,0,2,0.9,2,2s-0.9,2-2,2H4.2c-2.1,0-4,1.5-4.2,3.6C-0.2,60,1.7,62,4,62h53.8	c2.1,0,4-1.5,4.2-3.6C62.2,56,60.3,54,58,54z"
+                ></path>
+                <radialGradient
+                  id="TGwjmZMm2W~B4yrgup6jdb_119026_gr2"
+                  cx="18.51"
+                  cy="66.293"
+                  r="69.648"
+                  gradientTransform="matrix(.6435 -.7654 .5056 .4251 -26.92 52.282)"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop offset=".073" stop-color="#eacc7b"></stop>
+                  <stop offset=".184" stop-color="#ecaa59"></stop>
+                  <stop offset=".307" stop-color="#ef802e"></stop>
+                  <stop offset=".358" stop-color="#ef6d3a"></stop>
+                  <stop offset=".46" stop-color="#f04b50"></stop>
+                  <stop offset=".516" stop-color="#f03e58"></stop>
+                  <stop offset=".689" stop-color="#db359e"></stop>
+                  <stop offset=".724" stop-color="#ce37a4"></stop>
+                  <stop offset=".789" stop-color="#ac3cb4"></stop>
+                  <stop offset=".877" stop-color="#7544cf"></stop>
+                  <stop offset=".98" stop-color="#2b4ff2"></stop>
+                </radialGradient>
+                <path
+                  fill="url(#TGwjmZMm2W~B4yrgup6jdb_119026_gr2)"
+                  d="M45,57H19c-5.5,0-10-4.5-10-10V21c0-5.5,4.5-10,10-10h26c5.5,0,10,4.5,10,10v26C55,52.5,50.5,57,45,57z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M32,20c4.6,0,5.1,0,6.9,0.1c1.7,0.1,2.6,0.4,3.2,0.6c0.8,0.3,1.4,0.7,2,1.3c0.6,0.6,1,1.2,1.3,2 c0.2,0.6,0.5,1.5,0.6,3.2C46,28.9,46,29.4,46,34s0,5.1-0.1,6.9c-0.1,1.7-0.4,2.6-0.6,3.2c-0.3,0.8-0.7,1.4-1.3,2 c-0.6,0.6-1.2,1-2,1.3c-0.6,0.2-1.5,0.5-3.2,0.6C37.1,48,36.6,48,32,48s-5.1,0-6.9-0.1c-1.7-0.1-2.6-0.4-3.2-0.6 c-0.8-0.3-1.4-0.7-2-1.3c-0.6-0.6-1-1.2-1.3-2c-0.2-0.6-0.5-1.5-0.6-3.2C18,39.1,18,38.6,18,34s0-5.1,0.1-6.9 c0.1-1.7,0.4-2.6,0.6-3.2c0.3-0.8,0.7-1.4,1.3-2c0.6-0.6,1.2-1,2-1.3c0.6-0.2,1.5-0.5,3.2-0.6C26.9,20,27.4,20,32,20 M32,17 c-4.6,0-5.2,0-7,0.1c-1.8,0.1-3,0.4-4.1,0.8c-1.1,0.4-2.1,1-3,2s-1.5,1.9-2,3c-0.4,1.1-0.7,2.3-0.8,4.1C15,28.8,15,29.4,15,34 s0,5.2,0.1,7c0.1,1.8,0.4,3,0.8,4.1c0.4,1.1,1,2.1,2,3c0.9,0.9,1.9,1.5,3,2c1.1,0.4,2.3,0.7,4.1,0.8c1.8,0.1,2.4,0.1,7,0.1 s5.2,0,7-0.1c1.8-0.1,3-0.4,4.1-0.8c1.1-0.4,2.1-1,3-2c0.9-0.9,1.5-1.9,2-3c0.4-1.1,0.7-2.3,0.8-4.1c0.1-1.8,0.1-2.4,0.1-7 s0-5.2-0.1-7c-0.1-1.8-0.4-3-0.8-4.1c-0.4-1.1-1-2.1-2-3s-1.9-1.5-3-2c-1.1-0.4-2.3-0.7-4.1-0.8C37.2,17,36.6,17,32,17L32,17z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M32,25c-5,0-9,4-9,9s4,9,9,9s9-4,9-9S37,25,32,25z M32,40c-3.3,0-6-2.7-6-6s2.7-6,6-6s6,2.7,6,6S35.3,40,32,40 z"
+                ></path>
+                <circle cx="41" cy="25" r="2" fill="#fff"></circle>
               </svg>
             </a>
           </div>
 
           {/* Copyright */}
-          <div className="text-gray-400">
+          <div className="text-white dark:text-black">
             &copy; {new Date().getFullYear()} TutorBook. All rights reserved.
           </div>
         </div>
