b8c5aa13b618c2b1a834e55b6c9762fb58445de7
parthosarothi46 <parthosarothi46@gmail.com>
2024-12-26 19:58:21 +0600

    Loading Spinner done

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gray-100 dark:bg-gray-800">
-      <div className="loader border-t-4 border-black dark:border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="loader border-t-4 border-black dark:border-white rounded-full w-16 h-16 animate-spin"></div>
     </div>
   );
 };
